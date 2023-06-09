96999b2f10dbcd4bdb903d21a0c895fe0fdd650e
PIUS <relimoringa@gmail.com>
2023-06-10 00:19:48 +0300

    done 4 of them

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const cats =  ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    console.log(cats);
+}
+destructivelyAppendCat("MIlano");
+
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+    console.log(cats);
+}
+destructivelyPrependCat("Bee");
+
+
+function destructivelyRemoveLastCat(name) {
+
+    cats.pop();
+    console.log(cats);
+}
+destructivelyRemoveLastCat();
+
+
+function destructivelyRemoveFirstCat(name) {
+
+    cats.shift()
+    console.log(cats);
+}
+destructivelyRemoveFirstCat();
