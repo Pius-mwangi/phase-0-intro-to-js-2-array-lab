73726b02ef1bb39c2de57c537e4dfbff237ab679
PIUS <relimoringa@gmail.com>
2023-06-10 00:47:39 +0300

    completed

--- index.js
+++ index.js
@@ -1,30 +1,65 @@
 // Write your solution here!
-const cats =  ["Milo", "Otis", "Garfield"];
-function destructivelyAppendCat(name) {
-    cats.push(name);
-    console.log(cats);
-}
-destructivelyAppendCat("MIlano");
 
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+  console.log(cats);
+}
+
+destructivelyAppendCat("Milano");
 
 function destructivelyPrependCat(name) {
-    cats.unshift(name);
-    console.log(cats);
+  cats.unshift(name);
+  console.log(cats);
 }
+
 destructivelyPrependCat("Bee");
 
-
-function destructivelyRemoveLastCat(name) {
-
-    cats.pop();
-    console.log(cats);
+function destructivelyRemoveLastCat() {
+  cats.pop();
+  console.log(cats);
 }
+
 destructivelyRemoveLastCat();
 
-
-function destructivelyRemoveFirstCat(name) {
-
-    cats.shift()
-    console.log(cats);
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+  console.log(cats);
 }
+
 destructivelyRemoveFirstCat();
+
+function appendCat(name) {
+  const newCats = cats.slice();
+  newCats.push(name);
+  return newCats;
+}
+
+appendCat("Bee2");
+
+function prependCat(name) {
+  const newCats = cats.slice();
+  newCats.unshift(name);
+  return newCats;
+}
+
+prependCat("Bee1");
+
+function removeLastCat() {
+  const newCats = cats.slice();
+  newCats.pop();
+  
+  return newCats;
+}
+
+removeLastCat();
+
+function removeFirstCat() {
+  const newCats = cats.slice();
+  newCats.shift();
+ 
+  return newCats;
+}
+
+removeFirstCat();
